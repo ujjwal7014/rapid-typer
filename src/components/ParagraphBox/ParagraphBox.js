8bb670600d8def2b11107e57a2f83c633a20dfb2
ujjwal7014 <ujjwal7014@gmail.com>
2022-11-27 02:12:28 +0530

    basic structrue of paragraph Box

--- src/components/ParagraphBox/ParagraphBox.js
+++ src/components/ParagraphBox/ParagraphBox.js
@@ -1,12 +1,13 @@
 import './style.css';
 import React from 'react'
 
-export default function ParagraphBox(paragraph, userTypedText) {
-
+export default function ParagraphBox({ paragraph, userTypedText }) {
+  console.log("paragraph is ", paragraph)
   return (
-    <div className='paragraph-container'>
-      <h1>hey</h1>
-      <span>{paragraph}</span>
+    <div className='paragraph-wrapper'>
+      <div className='paragraph-container' >
+        <span>{paragraph || 'nothing'}</span>
+      </div>
     </div>
   )
 }
--- src/pages/GameArena/GameArena.js
+++ src/pages/GameArena/GameArena.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react'
 import ParagraphBox from '../../components/ParagraphBox/ParagraphBox'
 
 export default function GameArena() {
-  const [paragraph, setParagraph] = useState()
+  const fetchedParagraph = {
+    para: "ujjwal is a great guy he CSS justify-content property defines how the browser distributes space between and around content items along the main-axis of a flex container, and the inline axis of a grid container.The interactive example below demonstrates s"
+  }
+  const [paragraph, setParagraph] = useState('')
   const [userTypedText, setUserTypedText] = useState()
 
   useEffect(() => {
-    let fetchedParagraph = {para : "ujjwal is a great guy"}
     setParagraph(fetchedParagraph.para)
   }, [])
 
   return (
     <div>
       <ParagraphBox
-        pargraph={paragraph || {}}
+        paragraph={paragraph || ''}
         userTypedText={userTypedText || ''}
       />
     </div>
